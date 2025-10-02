c10bb63bf3fdac1a453632d72f813d1983254788
ktynk <ktyn26k@gmail.com>
2025-10-02 09:34:13 +0300

    Add files via upload

---
+++ 3 лаба задание 1.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h> 
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, v, x;
+	double z, i;
+	printf("введите начальное население:");
+	scanf_s("%lf", &i);
+	printf("введите натурала n:");
+	scanf_s("%d", &n);
+	i += 1. / n;
+	z = 3 * i;
+	x = 0;
+	while (i<z) {
+		i += 1. / n;
+		x++;
+		
+	}
+	printf("лет стоко:%d", x);
+	return 0;
+}
